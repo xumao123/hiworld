2f95dd88ef98a3b7d74d0676047e1fc90824570a
xumao123 <752948555@qq.com>
2019-04-09 11:13:42 +0800

    test

---
+++ src/HiWorld.java
@@ -0,0 +1,10 @@
+/**
+ * 2 * @Author: xumao
+ * 3 * @Date: 2019/4/9 0009 11:07
+ * 4
+ */
+public class HiWorld {
+    public static void main(String args[]){
+        System.out.println("Hi World");
+    }
+}
